de1c472aee13be65e0c56d7dba41e12b36c53d77
JonnyHightower <jonsoftware@funkygeek.com>
2014-04-17 15:08:54 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+NEET - Network Enumeration and Exploitation Tool
+
+RESOURCE REPOSITORY - NOTHING TO DO HERE
+
+This is a repository of resources required by Neet. Users DO NOT need
+to manually download this repository. It is automatically installed
+by neet-update whenever it changes.
+
+Neet is released under version 3 of the GNU Public License. See the LICENSE file for details.
+
+Copyright 2008-2014 Jonathan Roach
+Email: jonnyhightower [at] funkygeek.com
+
+
---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export NEET=/opt/neet
+export CONFIDR="${NEET}/etc"
+export VERSION=`cat VERSION`
+export INST="${PWD}"
+
+if [ ! -d "$NEET" ]; then
+	echo "Couldn't find neet installation. Exiting."
+	exit 1
+fi
+
+. ${NEET}/core/installsupport
+
+if [ ! -z $INVOKEDBYNEETUPDATE ] && [ $INVOKEDBYNEETUPDATE -eq 1 ]; then
+	FILESTOREMOVE=""
+	for file in $FILESTOREMOVE; do
+		rm -f "$file"
+	done
+	echo "   + Installing neet bundled package updates..."
+	#######################################################
+
+	for bin in iShellSQL ms08-067_check.py ndr.py ndr.pyc patator.py sadmindcmd.pl SQLaddaccount wkhtmltoimage wkhtmltoimage-amd64; do
+		cp content/$bin "${NEET}/pkg/bin/$bin"
+		echo "$bin=${NEET}/pkg/bin/$bin" >> "${CONFDIR}/locations"
+		chmod 755 "${NEET}/pkg/bin/$bin"
+	done
+
+	echo -n "   + "
+	# Nikto
+	PkgInstall nikto Nikto
+
+	#######################################################
+	newVersion neet-bundled $VERSION
+else
+	echo "This package is for the neet-update script and should not be installed manually."
+	exit 1
+fi
+
+exit 0
+
+
+
